4d74a5c6a3cf19fdb1851e302d8df82718ab4908
RedDev <thomhmail@gmail.com>
2023-02-02 16:33:26 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,85 @@
+//https://rblxwild.com/api/events/rain/join
+/*
+{
+    "captchaToken":"P0_eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwYXNza2V5IjoiTyt0K2Y2OG5RNEw4blBBdnpia2JldFJqcEZIY2ZrVG1GQWR2NERqNnFpR0p0Mm1JQmpiWE1IK3RRYmJyUGtVQWhKOWdsZ1Y5MnpGT0RBMFA4aUdPekxpOEdxamp1SDhrdDFBR2p4T1EvcDRTbVZhcFZ0TDlLZktITXpieXlmR1lrdW5hMWVoZU9mbDVjK2IzMGUwb1NZb3l5WCsyemtxc3pvVlJ0WG1kbStzR2s5MkhyMGViNUFYTmROZHZHaHRNRE1wREx5cnVoYjF5cWJMNTBWWDJweXV3MlJnMlVNa1I3a2kxMWZ2V1BWcUlpSTk4YVBZNURTdHhRc3Vpc2ZocHJZeitxL3cwQ1JETnllZ1BtbFFnRkFXVzJwOEtxUkVHOEpid3ppWkVPRFBZSmpJUU9lMWltVGFsYXVoTWR3NVg1cmlITFR6TXVrUU5iWmg2QjIwNjRYNjlER0RhT3poblhHN2JManpVOFIyV3NJcjRvdkx6UFlGckZpSkl6SmV0OVdlNzZsUFhsRmFuL0hPRW5rMGtQWWxuWVhySTJiVGNNaXcxQUt2dDNseitUb2lnWnMxSFlpeWh2U0dWQ2NxSDl1cnJoYVk0Z2Z4akNheDhINmF6RmU4Ymh2Q0dnVlo5NnRScHBhZ0tnM3JIRHdHVUtlYlMzRFZrRW5udUl1ai9sNlU0MnBiZXZHUVBGQ1ZCNlREWmN4ZTFFV0psNFN0bHUyMWFXeUl4b0FGdmFSTGxyVWtrUjVJeEVoQVFJSkVCK1AxVXVPRFZHVXZQaHhBN0lOeU9lTHJLbTBmckNZd3RoWklHdkpDeFV4dXd6YlpXY0IxVXY3dklqVS8xdkJiYTltZkw1Y0Q1ZGZuanlQallJTWRvbXhZSGNYaTlpWnI5bkZmbExXR3NqNkhlTkJTcFB6OVd2Wm1GS3ZlUTYvRmtqS3R0TmVNUnpxdVYycHhSUzNVSnNQcUpqNWcxNVZxUnpSc0Q1SjlkMzByM1k3OWhpRDFWNHN0MXJrbDZaWnhYMlNpdWZZeE9PemhEWk5JSXVIaHVtZmlUOW5uMStBdEZJN3dsVWhuVzc4aktINng4Y1lUSHNyZW44ajhRK1ZtSTJHazhwUGlXVUltek96Mnc4MjhVYTU2dmM4QTVYOEIzNkxkN2paOCtkQjdSOC92b2JqRG5YRTFaY01NUGEya241Uk05bDcwNzhqUDY4KytsUzZ2ZzkwdHJwMWExMldidTB6aWlWT0xCeU1reUZ2SkNKT1p0MmZQamhIZ2owMVhNei9PZ1JFUDFVUVJJZENjQ3VmbXlTZVpIdTBqdFMxdzVlNzVqb2ZQczZlTmZlODIzcGI5NUZyOHdjVVpBZ3R6M1JtcktFR1hLRFF6eFRnS2FHVkFKM1RJa3FzT3ladTZoQ2xvemYvUU9oQnR6ZmVrQ3A0dnF5aEFvQ3FZNlNNVFRhTmRSNTg3MHhTRFZVK3JBaG83QkJ3Y2lFdjJuT1RuZnB2bEN1bTNUTHlNaHM4ekNadVJaSEcyYWt3U245N3laNVdlNXRGYkp3b1ZFTXZqcC84Q1Q1SC80bnpoeTBpc0tRQWtLRU9VU0l3a3ZCbWhROVJnMDVkV0xHblh0cmx2bTJwZThrc2ZNcDhuZFF3TUVEajA0QlpWcjVrY1hhK3ZpU0YwZFpJeWZpbjd4d3JVeFM4VW85cm9jRy9CVnc2aGNmSm52bGNpeDByVGRJKy9JL1I0TlRCbGQyMWN0VXIwcHVQN2paMS9mSTJGRllMekR5V040RlZuSmRFbFo3SENkaW9KdFZFUGUvRXBnTS91OXNDdzdUV3ZwS0I3U1E5MUNmbkd4eHp6Zy9SdmZ4bDVZUUxsSWtsMHU1eXZCbjRiUHA1QjNMdjBJUmROV015STdRSTNKMVZKTlBqVXowNzhnRkhuZFpCYVZocUR3MXNyYUZGTmwvUXNhV1Fxb3REZzYxelhkTXVTZ1QwcTUvOE5vT1ZyMmY1eEE3d0E3ZmpTQVRCUk5DY3NSWWxZT0NoMFo5TkVCZUV1ZUNmWm1UNmNPaTYzeGNlRWhzMW9GeTV6VjZpNFdOY09UVmg3UFRjaGRRK3NzcEZNTlNMeXpTMzVUckZtZXI1Nlp3dnBna1VOWDhVbGt0SVJodHJUN3dGalNrSjh1RGk0OGw0Wlo2SldSeHRsQzBkY2RHOURUOFBnTjRlOTVQMG1UR1FKU0YxbTZ4d2c4Vk9VWGtLcUt2YVpzbHhjZ3p2bWlYWVREREFUanRQc0tEcDk5KzJYNmYzSER2aEJ2ZjdjQWNmeEpUc2VIaGVhL1p5Y1c4SDdCWEowYnl5SzFyZUMrZGkwVFVzRk1BcmN1MHBTc3RmVWVHL0pIOTRRYWpWTmJ1N1ZYckhCL1o4cEo1Ly9peUZXM2QwQVRQYWhpWStGOG1VS3F3UnhYd0xOckpUODhwbkxORnd2Mld2SjY5Y01UTmNQTDV6dnZsSWdEWkRUUm9DOTMvM2JhRlBEWWcwVEFBMURYV2MzaXVBaWZrWjhSaGxhd0k1TlBHWlRzSktoK2d2clcwZjBvZVFPYytzK3dWMVRxS3NUU0ZKV3lBWTd2UFdzYXZ0NnJ2bW1jSUx3SlBjTmxBN1ZIcVM0d1VuNjR3SUJoNjNQOFJwd2pZejlZN2ZBVTNQdFJCbk9GUVdFQ1JmRGJkdnZrMTNvU3c3YWJMUjIxQ0tJQjBwczlHamZZSzk5OGVDaVV5VXcyeFdvNXpRZklCSGR0VnAxdEtlVVBJZzlGKzUzS0ptS1VNczl6ZVpUdlpoZ2U4OXVTUzVnSHBhVzR3YTZuU3ZCNEpCcEJkK3J0djRXZ3h2dGdRRkZwaWl2Q2o0UHhJQ1dmNXhiVnd3SWtZVnNlVkpPTVl1VmhRekNpTWF1TnhCelBoOGk2SXhxbE03QlJhTDhraVlYVGYwUHJDUC9rRi9XODc2eEJUY0w0MFVoMzhhS3ZFMks0akdpcWxvY2FDaFA0YmRWaHBKSUpZaXJRTXdoQXVJdGJBY25NSDN0cXJsRytMb3NnN0pjcmVQVE5NZkV1WU0wNDczaFhEZzJ1STk1dDROOWo1TzR3eXM4a2lkWWF1T0tiQnJEMGhFc0hyd2NrZjRkbTlMTW1LbGNlZVAyanBkNjJJbFV2NHF6TXZDdFBaVHJqOXZZZlpJSFVseGZIQlE4U0FmRE85S3RqRFJMakZya1A5ZVpIUWxyS241bVBtYUVuN2V4d01PVUkraWNsTVdrcXVsd0M2a2tTWXg4YnFROW8rV25kSlhnMG9ISzEybkJrdjRiTE9yV3lKSlRhNkZQVE55dTB3bHFmbFBMNG91UWRyL3JjNDg3MENKWFBHNmJHdS9UMmZ1QmZWM0h4cnhuamVpN29uVzVYQ3dTV1NBSXdmNTZSUFBIT3JIR3RPa3VMN1ZEOEhTQW43OW1oNXdhUzI1Q0pnNDBzWWU0LzV3S211cXdUTGQzUlVrVzNualEzZ3Q3SkFzK0ZWWjhzUC90VWlVb0ZNa1R2UGI2RUdtb0ZPcnlxYmRGTEhJUXFuU3pxMHJPSjlmTC92aW1tVkpnR2FTZ3UxelV4dHdvVk5EZ2grajJIZGFMdUF1WW5YMUFIc1orZkdTRno2MThINy9ob1BscXlMMS9taXBwd0ticnRVVmttRG5CY3Y3U0tjNjhJRHp4U1pQblhtU0pOUytYZUphWmJmTzF0dmhXVFVveTR4dGlianpJRmRtRHh2eWVVdHowb0N6aThwcXlQcDFVUVJDcDUxOWJsRTBBeG4yQ1RCb0JobW9PM2xyMWNkQWhoWUdrT2tvYmQ4WndtemY5UCtMR0dLOTFiNXUyVDBWNW8vS2Z4U3VVT1U2K2c0ZURBdHhtZEdIMWhIRUNxbjVzS2o0WkdWRWdmWk9nb2NKNTNJREhPZkJ5QlpGZWliQThHSDc3dVFpSVZLdUt6cHBPek80cUswVVlTakRYWnk1ME9FVGNYVDlBSjV6VHlkNFFuTkZKVVE2QnRrWkUzemtvTkhSOUJCSG4vOENGRDlRLzFwbnV5ZDRJeC8wNnNDVkt6Ry84NDRFRncvOHc4ekpVK1lUZ2RoSzh3MXpwdjJyQWxqYXJHODNpR3FUQkhZbDcvK2lpM2ZLeDBxdm9XOUl4aUt5SGdaVjMvbFRadzZVZW0vdUFNVi93dUt4dGFKSW9qL09nTzBHVCtaMUM0QnB3Tm1zUTlxS1habmdkUUVxQ2kvbFl1T1Exd3owMjh4cmpiQk9KTnRlSGs1NXhsYW1QdXNWcG9LRjdKREk5VnhEYlY4V1EyR0c0MEVVcXNiNVBWd0JVeUxxY0lJS09XWk04U0Y1SnBnWHpaL0YzQ3krOU5CeHNiSDFlUWUzcFBaVFRFVnZGanVkMy9zTHJxR1JxUFVkMEhMUU1LOTdDY2JmV1p5ZDVZdVB5Qkpob28rdEdBUkIzNURRK0Y3K0R6dVpVa0xsUWhxUE9vZUE3MERXUGhnTXRKWDJBZnRhcmZlczczaTdxVnRqQWxWWlBsdWIyN3VabysrTUl5TFZtMHhzMGZmc3RYN0hzRzRQVzlnV2JxZE16UUtiYmMvdk41eWtLSmxlVC9SQlZ5WTdvaXpwNG5SSHV3SUtzRlJ3aWM4VWpnaG5OZGsxSEhVUkc3Wk4xQlVZNmxLZFB6d1R2Z0tUNkZ3VGZSc05CQkNhbkFXaVJuZUg0RmxGQUFKZmZRMGlXc1RvUUFPNzZHR3pjVW5jdUhmbDQ1dVJ1b1czak9GNzhkMzZ4Rzd6dzRDRDhLVEVJcCt0amxiamFXeTdlVDlXOEt1VFBtOHZKWVpnNThHdWd6MnlZeGp2MnAzMWFEdVU4aFROTDJkSFM2V3diNWw3MGZuM1J0OXI4RVFQMktLY0FVemZwMmFlTUlaR0k0MEN0UmVJMHF6SVBPdWJYNzQ2RWttbW9mTStPdE5LQnNyZnlnWTdlMjJXa3R1dFBmbzlDTk45WlI4dlhSNHRLbmZsMHluancxdlc1d0kvaWJzek9qcG01d09lOHhwamdBdTl3eThVSVhnMW55djRRM1dVLzhUaGZiazBKQmhXN1ZETzFQMmxrSktMK3dkL0RtaWo2c3I0SGoiLCJleHAiOjE2NjM2NzkyMzUsInNoYXJkX2lkIjozMzk1MTAzMDMsInBkIjowfQ.CuT8dnR7HvBES5_Vs7zYqWZhKAeGXzykAOoDcXkPCH8",
+    "potId":8173,
+    "i1oveu":true
+}
+*/
+const startTime1 = Date.now()
+const puppeteer = require("puppeteer")
+const axios = require("axios")
+const info_puller = require(__dirname + "/info-puller.js")
+const clui = require("clui")
+const colors = require("colors")
+const fs = require("fs")
+
+var pot = {
+    "id":0,
+    "lastUpdateMs":0
+}
+
+info_puller.launch(6101, "b6473716dfa7da831bde01cdd3eb8a855935bcf200428b731a1de9533bbb087dfc3efc90991ca75d41e6bdd9660e0b9275d08496a9f481cb6397e311d1f0afca")
+
+
+
+
+info_puller.onready(async()=>{
+    setInterval(async()=>{
+
+
+        var res = await info_puller.pullMsLeft()
+
+        var TIMEL = 0
+        if (res.succes){
+            if (res.started == false){
+                pot.lastUpdateMs = res.lastUpdateMs
+                pot.id = res.potid
+
+                TIMEL = (res.time/60000).toFixed(1)
+            } else {
+                TIMEL = 0
+            }
+        }
+        var ft = await fs.readFileSync(__dirname + "/templates/main.txt").toString()
+
+        var cd = TIMEL
+        var line = await clui.Sparkline(info_puller.lastPayouts, ' base-payout/rain ')
+
+
+        var Gauge = clui.Gauge;
+        var os = require("os")
+        var total = os.totalmem();
+        var free = os.freemem();
+        var used = total - free;
+        var human = Math.ceil(used / 1000000) + ' MB';
+        
+        
+
+
+        ft = await ft.replaceAll("%a%",colors.blue("Base payouts  ") + " " + line)
+        ft = await ft.replaceAll("%b%", cd)
+        ft = await ft.replaceAll("%c%","Total Memory In Use  ".blue + Gauge(used, total, 20, total * 0.8, human))
+        ft = await ft.replaceAll("%d%","Total session time  ".blue + ((await Date.now() - startTime1)/ 60000 ).toFixed(1))
+        ft = await ft.replaceAll("%e%", "Average response time  ".blue + clui.Sparkline(info_puller.avResponse," time/ping "))
+        ft = await ft.replaceAll("%f%","Amount websocket closed: ".blue + info_puller.amountclosed)
+
+        console.clear()
+        console.log(ft + "\n")
+
+    },5000)
+})
+
+info_puller.onmessage(async(msg)=>{
+    if (msg[0] == "events:rain:setState"){
+        //{ newState: 'ENDING', lastUpdateMs: 1663832539646 }
+        if (msg[1].newState == "ENDING"){
+            //Pot ending soon
+        }
+    }
+    if(msg[0] == "events:rain:setPot"){
+      //new pot created
+    }
+})
+
+
